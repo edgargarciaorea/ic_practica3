143dc698762d07d52a1e25bdf0724ef5fa14b2be
edgar <edgargarciaorea@gmail.com>
2021-11-29 11:04:14 +0100

    Agregada carpeta build y su makefile

---
+++ code/build/CMakeFiles/cv.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ic/Escritorio/ic_tarea3_finetunnig/code/crossValidation.cc" "/home/ic/Escritorio/ic_tarea3_finetunnig/code/build/CMakeFiles/cv.dir/crossValidation.cc.o"
+  "/home/ic/Escritorio/ic_tarea3_finetunnig/code/knn.cc" "/home/ic/Escritorio/ic_tarea3_finetunnig/code/build/CMakeFiles/cv.dir/knn.cc.o"
+  "/home/ic/Escritorio/ic_tarea3_finetunnig/code/main.cc" "/home/ic/Escritorio/ic_tarea3_finetunnig/code/build/CMakeFiles/cv.dir/main.cc.o"
+  "/home/ic/Escritorio/ic_tarea3_finetunnig/code/mnist.cc" "/home/ic/Escritorio/ic_tarea3_finetunnig/code/build/CMakeFiles/cv.dir/mnist.cc.o"
+  "/home/ic/Escritorio/ic_tarea3_finetunnig/code/sample.cc" "/home/ic/Escritorio/ic_tarea3_finetunnig/code/build/CMakeFiles/cv.dir/sample.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/build/CMakeFiles/cv.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv.dir/crossValidation.cc.o"
+  "CMakeFiles/cv.dir/knn.cc.o"
+  "CMakeFiles/cv.dir/main.cc.o"
+  "CMakeFiles/cv.dir/mnist.cc.o"
+  "CMakeFiles/cv.dir/sample.cc.o"
+  "cv"
+  "cv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
